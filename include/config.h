956bf95375a8c84e5aa46bb14b33df4cc17810c0
MDRobiulHAASAN <md.robiul77dhf@gmail.com>
2025-11-24 00:48:16 +0600

    Add WiFi configuration and security settings

---
+++ include/config.h
@@ -0,0 +1,8 @@
+// WiFi Configuration
+#define AP_SSID "HACKER"
+#define AP_PASSWORD "12345678"
+#define AP_IP "192.168.4.1"
+
+// Security Settings
+#define MAX_NETWORKS 32
+#define SCAN_INTERVAL 15000
